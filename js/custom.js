6473dd149faed7f821f2049f75c89adc7008a2e4
bronisMateusz <mateusz.bronis99@gmail.com>
2020-10-02 11:06:20 +0200

    Anims were changed and some fonts were removed.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
 AOS.init({
     duration: 1400
 })
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar.fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
