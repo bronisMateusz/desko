773c5b1a11b386da9d33f1752bc8ae756165be18
bronisMateusz <mateusz.bronis99@gmail.com>
2020-10-30 08:55:38 +0100

    Added zoom on hovered product pictures.

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,47 @@ $(function () {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
 })
+
+/*const productWrapper = document.getElementById('product-wrapper')
+const productImg = document.getElementById('product-img');
+
+productWrapper.addEventListener('mousemove', function(e){
+  let width = productWrapper.offsetWidth;
+  let height = productWrapper.offsetHeight;
+  let mouseX = e.offsetX;
+  let mouseY = e.offsetY;
+
+  console.log(mouseX + ' : '+mouseY);
+
+  let imgPosX = (mouseX/width*100);
+  let imgPosY = (mouseY/height*100);
+
+  productImg.style.transform = `translate(-${imgPosX}%,-${imgPosY}%) scale(2)`;
+})
+
+productWrapper.addEventListener('mouseleave', function(){
+  productImg.style.transform = 'translate(-50%,-50%) scale(1)';
+})*/
+
+const productWrapper = document.getElementsByClassName('product-img-wrapper')
+const productImg = document.getElementsByClassName ('product-img');
+
+for(let i=0;i<productWrapper.length;i++){
+  productWrapper[i].addEventListener('mousemove', function(e){
+    let width = productWrapper[i].offsetWidth;
+    let height = productWrapper[i].offsetHeight;
+    let mouseX = e.offsetX;
+    let mouseY = e.offsetY;
+  
+    let imgPosX = (mouseX/width*100);
+    let imgPosY = (mouseY/height*100);
+    
+    productImg[i].style.transform = `translate(-${imgPosX}%,-${imgPosY}%) scale(2)`;
+  })
+
+  productWrapper[i].addEventListener('mouseleave', function(){
+    productImg[i].style.transform = 'translate(-50%,-50%) scale(1)';
+  })
+
+  console.log(i);
+}
