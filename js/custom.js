1bf49677be81549c3779c66b3d66843784adea8b
bronisMateusz <mateusz.bronis99@gmail.com>
2020-11-22 00:52:05 +0100

    Lorem ipsum replaced and script added to jumbotron

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+//_ HEADER COLOR CHANGE //
 $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar.fixed-top");
@@ -5,10 +6,11 @@ $(function () {
     });
 })
 
-const productWrapper = document.getElementsByClassName('product-img-wrapper')
+//_ OFFER IMG ZOOMER //
+const productWrapper = document.getElementsByClassName('product-img-wrapper');
 const productImg = document.getElementsByClassName ('product-img');
 
-for(let i=0;i<productWrapper.length;i++){
+for(let i=0; i<productWrapper.length; i++){
   productWrapper[i].addEventListener('mousemove', function(e){
     let width = productWrapper[i].offsetWidth;
     let height = productWrapper[i].offsetHeight;
@@ -26,6 +28,27 @@ for(let i=0;i<productWrapper.length;i++){
   })
 }
 
+//_ JUMBOTRON IMAGE CHANGER //
+let jumbotron = document.querySelector('.jumbotron');
+let bgImg = [];
+let bgIndex = 0;
+
+bgImg[0] = "url('pictures/slide1.jpg')";
+bgImg[1] = "url('pictures/slide2.jpg')";
+
+function jumboSlider(){
+  if(bgIndex>bgImg.length-1){
+    bgIndex = 0;
+  }
+  jumbotron.style.backgroundImage = bgImg[bgIndex];
+  bgIndex++;
+
+  setTimeout(jumboSlider,5000);
+}
+window.onload = jumboSlider();
+
+
+//_ AOS //
 AOS.init({
   duration: 1400
 })
